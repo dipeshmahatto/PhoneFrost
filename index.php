dc8ebf0aed442971878c21336531bfbb5c079720
dipesh <dipeshmahatto@gmail.com>
2024-09-19 23:11:13 +0545

    landing page modified

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,31 +10,24 @@
     <link rel="stylesheet" href="css/contetDetails.css">
 
     <title> PhoneForst</title>
-    <!-- favicon -->
     <link rel="icon" href="img/icon.png" type="image/gif" sizes="16x16">
-    <!-- header links -->
     <script src="https://kit.fontawesome.com/4a3b1f73a2.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <!-- slider links -->
     <script src="js/jQuery3.4.1.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
 </head>
 
 <body>
-    <!-- HEADER -->
     <div id="1"><?php include("header.php") ?></div>
-    <!-- SLIDER -->
+
     <div id="2"><?php include("slider.php") ?></div>
 
-    <!-- CONTENT SECTION -->
     <div id="3"><?php include("content.php") ?></div>
 
-    <!-- FOOTER -->
     <div id="4"><?php include("footer.php") ?></div>
 </body>
 
-<!-- slider JS START -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <script>
     $(document).ready(function () {
